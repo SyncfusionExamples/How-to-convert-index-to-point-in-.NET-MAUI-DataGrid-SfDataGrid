bba6d82b6adf5a23a872abef53e75a702e8bd43a
Sudarsan Muthuselvan <sudarsan.m@syncfusion.com>
2023-12-11 18:50:10 +0530

    sample

---
+++ SfDataGridSample/MainPage.xaml.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.Maui;
+using Microsoft.Maui.Controls.Compatibility;
+using Syncfusion.Maui.Data;
+using Syncfusion.Maui.DataGrid;
+using Syncfusion.Maui.DataGrid.Helper;
+using System.Diagnostics;
+
+namespace SfDataGridSample
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void Datagrid_CellTapped(object? sender, DataGridCellTappedEventArgs e)
+        {
+            Point point = datagrid.RowColumnIndexToPoint(e.RowColumnIndex);
+            Debug.WriteLine(point.X + " " + point.Y);
+        }       
+    }
+}
